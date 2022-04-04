7c3d8395a2803268b74bc311da1c484d7fd57f25
Jack Paget <jackpaget1@gmail.com>
2022-04-04 14:40:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Testimonials-slider
+  This is an example page produced for Frontend Mentor 
---
+++ app.js
@@ -0,0 +1,30 @@
+const next = document.querySelector('#next-btn');
+const prev = document.querySelector('#prev-btn');
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+display(index);
+function display(index) {
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+  slides[index].style.display = 'flex';
+}
+
+function nextSlide() {
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+  display(index);
+}
+function prevSlide() {
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  display(index);
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
